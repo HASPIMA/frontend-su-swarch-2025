6dcd7f080aa52c9066a71a6a7e77151ae7ed4860
Harrison Pinto <hpintom@unal.edu.co>
2025-06-01 13:42:21 -0500

    Fix types in next config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@
  * for Docker builds.
  */
 import "./src/env.ts";
+import type { NextConfig } from "next";
 
-/** @type {import("next").NextConfig} */
-const config = {};
+const config: NextConfig = {};
 
 export default config;
